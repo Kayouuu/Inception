da685ba3cbc700f5a0c7d447102529b3d187f7fd
psaulnie <psaulnie@student.42lyon.fr>
2022-10-17 16:26:46 +0200

    ca marchegit add *git add *git add *git add *

---
+++ srcs/requirements/mariadb/tools/start.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+exec /usr/bin/mysqld_safe
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME}; \
+CREATE USER ${MYSQL_USER}@localhost IDENTIFIED BY '${MYSQL_PASSWORD}'; \
+GRANT ALL ON wordpress.* TO ${MYSQL_USER}@localhost; \
+FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
